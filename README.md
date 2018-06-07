79844f5c5bb05e70ebed561d2bb99574ccaebd1f
gatemediang <estolog@gmail.com>
2018-06-07 03:51:32 +0100

    express server

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# ShoppingListAPI
-Shopping List API
+
+
+## SHOPPING LIST API
+
+
+
+### Installation and Running the App
+
+Make sure `nodemon` is installed globally:
+
+```
+npm install -g nodemon
+```
+
+Clone the repo, then:
+
+```
+npm run dev
+```
+
+The app will be served at `localhost:3000`.
---
+++ server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+});
+
+const hostname = 'localhost';
+const port = 3001;
+
+const server = app.listen(port, hostname, () => {
+
+  console.log(`Server running at http://${hostname}:${port}/`);
+  
+});
